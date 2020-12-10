b9585501e70dde07e8ccf65ba68b8a8dd52b6054
SergeyRazgulyaev <razgulyaev.sergey@gmail.com>
2020-12-10 12:24:21 +0300

    Lesson 01 Home Work v3
    Lesson 01 Home Work v3

--- Lesson01HomeWork/Calculator/Calculator/main.m
+++ Lesson01HomeWork/Calculator/Calculator/main.m
@@ -1,8 +1,8 @@
 //
 //  main.m
-//  Lesson01HomeWork
+//  Calculator
 //
-//  Created by Sergey Razgulyaev on 07.12.2020.
+//  Created by Sergey Razgulyaev on 10.12.2020.
 //
 
 #import <Foundation/Foundation.h>
@@ -13,11 +13,6 @@ int main(int argc, const char * argv[]) {
         int firstNumber = 0;
         int secondNumber = 0;
         
-        //Variables for calculating the average
-        float firstNumberForAverageCalc = 0.0;
-        float secondNumberForAverageCalc = 0.0;
-        float thirdNumberForAverageCalc = 0.0;
-
         //User input of two numbers for input/output and arithmetic operations on two numbers
         printf("Input/output and arithmetic operations on two numbers:\nEnter two numbers!\n");
         printf("First number = ");
@@ -55,19 +50,6 @@ int main(int argc, const char * argv[]) {
                   firstNumber, secondNumber, firstNumber * secondNumber);
             printf("You cannot divide by 0!\n");
         }
-        
-        //User input of three numbers for calculating the average
-        printf("\nCalculating the average:\nEnter three numbers!\n");
-        printf("First number = ");
-        scanf("%f", &firstNumberForAverageCalc);
-        printf("Second number = ");
-        scanf("%f", &secondNumberForAverageCalc);
-        printf("Third number = ");
-        scanf("%f", &thirdNumberForAverageCalc);
-        
-        //Calculating the average
-        float average = (firstNumberForAverageCalc + secondNumberForAverageCalc + thirdNumberForAverageCalc) / 3;
-        NSLog(@"Average for numbers %.3f, %.3f, %.3f = %.3f", firstNumberForAverageCalc, secondNumberForAverageCalc, thirdNumberForAverageCalc, average);
     }
     return 0;
 }
